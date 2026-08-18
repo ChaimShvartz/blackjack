f8dc84c235d5ef4b953c323419eb6fa6597577ec
Chaim <c0507634153@gmail.com>
2026-08-18 20:11:42 +0300

    Add get my-round endpoint

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,11 +1,25 @@
 import playersRepo from "../repositorues/players.repo.js";
 import roundsRepo from "../repositorues/rounds.repo.js";
 
-export  function createPlayer() {
+export async function createPlayer(_req, res) {
     const chips = process.env.STARTING_CHIPS;
     const newPlayer = {
         chips,
         createdAt: new Date().toLocaleDateString("he-IL"),
     };
-    return newPlayer;
+    const playerId = await playersRepo.insertPlayer(newPlayer);
+    res.status(201).json({ playerId, chips });
+}
+
+export async function createRound(req, res) {}
+
+export async function getRound(/**@type {Request}) */ req, res) {
+    const playerId = req.headers["x-player-id"];
+    const round = await roundsRepo.getRoundByPlayerId(playerId);
+    if (round) {
+        const { playerId, ...rest } = round;
+        res.json(rest);
+    } else {
+        res.json({ round: null });
+    }
 }
--- routes.js
+++ routes.js
@@ -1,19 +1,19 @@
 import { Router } from "express";
 import playersRepo from "./repositorues/players.repo.js";
 import roundsRepo from "./repositorues/rounds.repo.js";
-import { createPlayer } from "./controllers/controller.js";
+import {
+    createPlayer,
+    createRound,
+    getRound,
+} from "./controllers/controller.js";
 export const router = Router();
 
 router.get("/", (req, res) => {
     res.sendFile(process.cwd() + "/index.html");
 });
 
-router.post("/start-game", async (req, res) => {
-    const newPlayer = createPlayer();
-    const playerId = await playersRepo.insertPlayer(newPlayer);
-    const { chips } = newPlayer;
-    res.status(201).json({ playerId, chips });
-});
+router.post("/start-game", createPlayer);
+router.post("/start-round", createRound);
 router.post("/hit", (req, res) => {});
 router.post("/stand", (req, res) => {});
-router.get("/my-round", (req, res) => {});
+router.get("/my-round", getRound);
